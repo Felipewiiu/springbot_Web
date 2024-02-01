1e53933eda14b00f25ce8a46b08d91145abe22b4
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2024-02-01 06:58:55 -0300

    feat: Criando a busca por id

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,11 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos(){
         return servico.obterLancamento();
+    }
+
+    @GetMapping("/{id}")// uma parâmetro de uma URL é passado por chaves
+    public SerieDTO obterSerie(@PathVariable Long id){
+        return servico.obterPorId(id);
     }
 
 
